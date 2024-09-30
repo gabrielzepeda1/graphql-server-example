fa6ac556b007de73ba722925f35fe59a2e1240a8
gabrielzepeda1 <gabrielzepeda846@gmail.com>
2024-09-30 12:50:53 -0600

    adding more resolvers

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,27 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema";
 import data from "./data";
+import { doTypesOverlap } from "graphql";
 
 const resolvers = {
   Query: {
     games() {
       return data.games;
     },
+    game(_: any, args: { id: string }) {
+      return data.games.find((game) => game.id === args.id);
+    },
     authors() {
       return data.authors;
     },
+    author(_: any, args: { id: string }) {
+      return data.authors.find((author) => author.id === args.id);
+    },
     reviews() {
       return data.reviews;
+    },
+    review(_: any, args: { id: string }) {
+      return data.reviews.find((review) => review.id === args.id);
     },
   },
 };
